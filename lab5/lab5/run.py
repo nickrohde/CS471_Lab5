1ce8e953d89912c88c093a5ef8f82111dea8d839
nickrohde <rohden@cwu.edu>
2018-05-24 14:35:58 -0700

    finished script

--- lab5/lab5/run.py
+++ lab5/lab5/run.py
@@ -1,14 +1,53 @@
 import subprocess
+import os
 
 
+var = "-1"
 
-print "Starting run ...\n"
+if not os.path.isfile("main"):
+	subprocess.call(["make", "all"])
+if not os.path.isfile("main"):
+	print "Make failed! Terminating ..."
+	subprocess.call(["make", "clean"])
+	sys.exit()
 
 
-subprocess.call(["make", "all"])
+while var != "Q" and var != "q":
+	while var != "1" and var != "2" and var != "Q" and var != "q":
+		print "Pleas enter choice: "
+		print "1: Run all"
+		print "2: Run specific"
+		print "Q: Quit"
 
-for i in range(1,121,1):
-    subprocess.call(["./main", str(i)])
+		try:
+			var = raw_input("Choice: ")
+		except:
+			print "Please enter 1, 2, or Q/q."
+			var = "-1"
 
-print "Finished. Cleaning up ...\n"    
+	if var == "1":
+		var = "-1"
+		print "Starting run ...\n"
+
+		for i in range(1,121,1):
+			subprocess.call(["./main", str(i)])
+
+	elif var == "2":
+		var = "-1"
+		var2 = -1
+		while not (var2 >= 1 and var2 <= 120):
+			print "Which problem? (1-120): "
+			try:
+				var2 = input("Choice: ")
+			except:
+				print "Please enter an integer in the range [1,120]."
+				var2 = -1
+
+		print "Starting run ...\n"		
+		subprocess.call(["./main", str(var2)])
+
+print "Cleaning up ...\n"    
+
 subprocess.call(["make", "clean"])
+
+print "Finished."
